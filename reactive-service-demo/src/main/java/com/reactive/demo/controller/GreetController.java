4eb77a7d08d40bdb725371894c7e6fd3460b1a5b
Mukul <rmukul88@gmail.com>
2022-10-05 19:52:20 +0530

    Updated the rest controller as non-blocking code

--- reactive-service-demo/src/main/java/com/reactive/demo/controller/GreetController.java
+++ reactive-service-demo/src/main/java/com/reactive/demo/controller/GreetController.java
@@ -5,11 +5,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @RestController
 public class GreetController {
 
     @GetMapping("/greet")
-    public Mono<Greeting> getGreetings(){
-        return Mono.just(new Greeting("Hello From Rest Controller !!"));
+    public Mono<Greeting> getGreetings() {
+        return computeGreet();
+    }
+
+    @GetMapping("/name")
+    public Mono<String> getNameFromDb() {
+        return getName();
+    }
+    private Mono<Greeting> computeGreet() {
+        return Mono.just(new Greeting("Hello From Rest Controller !!")).delayElement(Duration.ofSeconds(5));
+    }
+    private Mono<String> getName(){
+        return Mono.just("Pratap").delayElement(Duration.ofSeconds(10));
     }
 }
